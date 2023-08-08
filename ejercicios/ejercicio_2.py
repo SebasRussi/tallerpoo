5c029f399ba018246ff817ba64dd8b3190529ab2
CodeDasg <dsanabriagomez@outlook.es>
2023-08-08 13:09:37 -0500

    ejercicio 2

--- ejercicios/ejercicio_2.py
+++ ejercicios/ejercicio_2.py
@@ -1,7 +1,9 @@
 class Punto:
-    def __init__(self, eje_x: int, eje_y: int):
+    def __init__(self, eje_x: int, eje_y: int, otro_ejex, otro_ejey):
         self.eje_x: int = eje_x
         self.eje_y: int = eje_y
+        self.otro_ejex = otro_ejex
+        self.otro_ejey = otro_ejey
 
     def mostrar(self):
         print(self.eje_x, self.eje_y)
@@ -12,10 +14,13 @@ class Punto:
         print(nuevo_eje_x, nuevo_eje_y)
 
     def calcular_distancia(self):
-        pass
+        distancia = (((self.otro_ejex - self.eje_x) ** 2) + ((self.otro_ejey - self.eje_y) ** 2)) ** 0.5
+        print(distancia)
 
 
 if __name__ == "__main__":
-    p1 = Punto(5, 3)
+    p1 = Punto(-4, -3, 2, 5)
+
     p1.mostrar()
     p1.mover(8, 11)
+    p1.calcular_distancia()
