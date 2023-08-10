34691403180da96809d96b9dba2261a08b366546
CodeDasg <dsanabriagomez@outlook.es>
2023-08-10 12:17:47 -0500

    ejercicio 5

--- ejercicios/ejercicio 6.py
+++ ejercicios/ejercicio 6.py
@@ -1,9 +1,17 @@
 class Carta:
-    DIAMANTES = "DIAMANTES"
-    CORAZONES = "CORAZONES"
-    TREVORES = "TREVORES"
-    ESPADAS = "ESPADAS"
+    DIAMANTE = "\u2663"
+    CORAZON = "\u2764"
+    TREBOL = "\u2666"
+    ESPADA = "\u2660"
 
     def __init__(self, valor, pinta):
         self.valor = valor
         self.pinta = pinta
+
+    def mostrar(self):
+        print(f"{self.valor}{self.pinta}")
+
+
+if __name__ == "__main__":
+    carta1 = Carta("A", Carta.TREBOL)
+    carta1.mostrar()
