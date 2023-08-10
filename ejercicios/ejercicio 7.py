1cf05e405e567942b3ce236f2e8a20a4c5582e85
CodeDasg <dsanabriagomez@outlook.es>
2023-08-10 12:39:30 -0500

    ejercicio 7 8 9 10 y 11

---
+++ ejercicios/ejercicio 7.py
@@ -0,0 +1,17 @@
+class CuantaBancaria:
+    def __init__(self, numero_cuenta, propietarios, balance):
+        self.numero_cuena = numero_cuenta
+        self.propietarios = propietarios
+        self.balance = balance
+
+    def depositar(self):
+        pass
+
+    def retirar(self):
+        pass
+
+    def aplicar_cuota(self):
+        pass
+
+    def mostrar_detalles(self):
+        pass
